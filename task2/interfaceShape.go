45948fbbe0d0f510e76cb1e91dc68a0b911491b2
martintree <miaozhiyong@itcast.cn>
2025-11-28 15:15:56 +0800

    理解值接收者和指针接收者

--- task2/interfaceShape.go
+++ task2/interfaceShape.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Shape interface {
 	Area()
@@ -15,6 +17,20 @@ type Circle struct {
 	name string
 }
 
+type Triangle struct {
+	name string
+}
+
+// 注意这里是值传参
+func (r Triangle) Area() {
+	fmt.Printf("invoke %s's Area()\n", r.name)
+}
+
+func (r Triangle) Perimeter() {
+	fmt.Printf("invoke %s's Perimeter()\n", r.name)
+}
+
+// 注意这里是指针传参
 func (r *Rectangle) Area() {
 	fmt.Printf("invoke %s's Area()\n", r.name)
 }
@@ -33,11 +49,30 @@ func (c *Circle) Perimeter() {
 
 func main() {
 	rectangle := &Rectangle{"Rectangle"}
-	circle := &Circle{"Circle"}
+	circle := Circle{"Circle"}
+	triangle := Triangle{"Triangle"}
 
-	rectangle.Area()
+	//语法糖，自动取地址,实际上是：(&triangle)
+	(&triangle).Area()
+
+	var s Shape = (&triangle)
+	s.Area()
+
+	fmt.Println("=========")
+	//语法糖， 自动解引用，实际上是：(*rectangle)所以下面这句也可以
+	(*rectangle).Area()
 	rectangle.Perimeter()
 
 	circle.Area()
 	circle.Perimeter()
+
+	var shapes []Shape
+	shapes = append(shapes, rectangle)
+	shapes = append(shapes, &circle)
+	shapes = append(shapes, triangle)
+	fmt.Println("=========")
+	for _, shape := range shapes {
+		shape.Area()
+		shape.Perimeter()
+	}
 }
